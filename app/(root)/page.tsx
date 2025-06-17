edc0b75cdb73bbbcaa823596460823baca5a121b
amine <amine.bouazzi2021@gmail.com>
2025-06-17 09:56:50 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+
+const Home = () => {
+  return (
+    <>
+      <p className="bg">Home</p>
+      <header className="flex justify-end items-center  p-4 gap-4 h-16">
+       
+      </header>
+    </>
+  );
+};
+
+export default Home;
