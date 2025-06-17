b543c5ca164fdbaf0416b58c042854a6f30bedb8
amine <amine.bouazzi2021@gmail.com>
2025-06-17 10:29:24 +0100

    fix the eslint building problem by configuring the next.config.ts file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
 
-
 const Home = () => {
   return (
     <>
       <p className="bg">Home</p>
-      <header className="flex justify-end items-center  p-4 gap-4 h-16">
-       
-      </header>
+      <header className="flex justify-end items-center  p-4 gap-4 h-16"></header>
     </>
   );
 };
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,44 +1,43 @@
-import { model, models, Schema } from "mongoose";
-
-import { Types } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: Record<string, any>;
+  config?: object;
   transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author?: {
+  author: {
     _id: string;
     firstName: string;
     lastName: string;
   };
   createdAt?: Date;
-  updatedat?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
   author: { type: Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
-  updatedat: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
+
 const Image = models?.Image || model("Image", ImageSchema);
 
 export default Image;
